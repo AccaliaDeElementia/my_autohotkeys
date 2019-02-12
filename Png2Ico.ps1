381c58a010c34a4677be9c265b1403ff950d838e
Accalia <accalia@elementia.me>
2019-02-12 10:51:11 -0500

    change icon conversion script to handle transparency

--- Png2Ico.ps1
+++ Png2Ico.ps1
@@ -1,9 +1,6 @@
-forEach ($file in Get-ChildItem . -Filter *.png) { 
+forEach ($file in Get-ChildItem . -Filter *.png) {
     & 'C:\Program Files\ImageMagick-7.0.8-Q16\magick.exe' `
         convert .\$file `
-        -resize 64x64 `
-        -gravity center `
-        -extent 64x64 +repage `
         -define icon:auto-resize=64,48,62,16 `
-        .\$($file.Basename).ico
+        ..\$($file.Basename).ico
 }
